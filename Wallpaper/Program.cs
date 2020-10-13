9c17d8579ce43b98f7d41d46c430da091eb8d62d
Travis Wooley <Travis.Wooley@ahu.edu>
2020-10-13 17:28:24 -0400

    Save as compressed jpg and delete original when cropping

--- Wallpaper/Program.cs
+++ Wallpaper/Program.cs
@@ -169,15 +169,32 @@ namespace Wallpaper
                     }
                     try
                     {
+                        ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+
+                        // Create an Encoder object based on the GUID  
+                        // for the Quality parameter category.  
+                        System.Drawing.Imaging.Encoder myEncoder =  
+                            System.Drawing.Imaging.Encoder.Quality;  
+            
+                        // Create an EncoderParameters object.  
+                        // An EncoderParameters object has an array of EncoderParameter  
+                        // objects. In this case, there is only one  
+                        // EncoderParameter object in the array.  
+                        EncoderParameters myEncoderParameters = new EncoderParameters(1);  
+            
+                        EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, 80L);  
+                        myEncoderParameters.Param[0] = myEncoderParameter;  
+            
                         Bitmap bmpImage = new Bitmap(file);
-                        ResizeImage(bmpImage.Clone(crop, bmpImage.PixelFormat), width, height).Save(crop_file);
+                        ResizeImage(bmpImage.Clone(crop, bmpImage.PixelFormat), width, height).Save(crop_file, jpgEncoder, myEncoderParameters);
+                        bmpImage.Dispose();
+                        File.Delete(file);
+                        File.Move(crop_file, file);
                     }
                     catch
                     {
                         Environment.Exit(0);
                     }
-
-                    file = crop_file;
 				}
 
                 if (monitor == uint.MaxValue)
@@ -190,7 +207,18 @@ namespace Wallpaper
                 }
             }
         }
-
+        private static ImageCodecInfo GetEncoder(ImageFormat format)  
+        {  
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();  
+            foreach (ImageCodecInfo codec in codecs)  
+            {  
+                if (codec.FormatID == format.Guid)  
+                {  
+                    return codec;  
+                }  
+            }  
+            return null;  
+        }  
         private static void SetAsWallPaper(string file)
         {
             try
